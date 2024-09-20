4538359d390a2dcb8bd62d0153c95e7aa31404a7
wit <witwitchayakarn@gmail.com>
2024-09-20 11:08:22 +0700

    Update brace-docs.org

--- bin/deploy-prod.sh
+++ bin/deploy-prod.sh
@@ -6,6 +6,15 @@ aws s3 sync out/nextImageExportOptimizer s3://brace-docs/nextImageExportOptimize
 
 aws s3 sync out s3://brace-docs --exclude "_next/*" --exclude "nextImageExportOptimizer/*" --exclude "images/next-image-export-optimizer-hashes.json" --size-only --delete --cache-control max-age=86400
 
+# Use CloudFront Function instead to map url path to html file
+#(cd out &&
+#  find . -type f -name '*.html' | while read HTMLFILE; do
+#    HTMLFILESHORT=${HTMLFILE:2}
+#    HTMLFILE_WITHOUT_EXT=${HTMLFILESHORT::${#HTMLFILESHORT}-5}
+#
+#    aws s3 cp ${HTMLFILE} s3://brace-docs/$HTMLFILE_WITHOUT_EXT --content-type "text/html" --cache-control max-age=86400
+#  done)
+
 # Must invalidation index.html and index.txt
 #aws cloudfront create-invalidation --distribution-id [!!!] --paths "/*"
 #aws cloudfront create-invalidation --distribution-id [!!!] --paths /index.html
